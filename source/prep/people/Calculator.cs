06aa71a8966f09293a46494e512e7becbc5b4b1f
Clayton Sayer <cbigsby@gmail.com>
2017-03-21 16:07:00 -0700

    Pushing new changes

--- source/prep/people/Calculator.cs
+++ source/prep/people/Calculator.cs
@@ -1,19 +1,28 @@
 ﻿using System;
+using System.Data;
 
 namespace code.prep.people
 {
-  public interface ICalculate
-  {
-    int add(int i, int i1);
-  }
-
-  public class Calculator : ICalculate
-  {
-    public int add(int i, int i1)
+    public interface ICalculate
     {
-      if (i < 0 || i1 < 0) throw new NotImplementedException();
-      return i + i1;
+        int add(int i, int i1);
     }
 
-  }
+    public class Calculator : ICalculate
+    {
+        private readonly IDbConnection _connection;
+
+        public Calculator(IDbConnection connection)
+        {
+            _connection = connection;
+            _connection.Open();
+        }
+
+        public int add(int i, int i1)
+        {
+            if (i < 0 || i1 < 0) throw new NotImplementedException();
+            return i + i1;
+        }
+
+    }
 }
