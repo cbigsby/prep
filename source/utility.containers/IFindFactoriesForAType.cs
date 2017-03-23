8d3c322d66658c78ce37d2693aeae2be673fd4e0
Clayton Sayer <cbigsby@gmail.com>
2017-03-23 16:21:59 -0500

    Pushing new changes

--- source/utility.containers/IFindFactoriesForAType.cs
+++ source/utility.containers/IFindFactoriesForAType.cs
@@ -2,7 +2,6 @@
 
 namespace code.utility.containers
 {
-    public delegate TypeToCreate TypeBuilder<out TypeToCreate>();
     public interface IFindFactoriesForAType
     {
         TypeBuilder<TypeToCreate> get_resolver_for_type<TypeToCreate>();
