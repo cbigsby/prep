3a72255fdddbb1f594bcf7c0da87ee4ab7b322b2
Clayton Sayer <cbigsby@gmail.com>
2017-03-23 16:26:11 -0500

    Pushing new changes

--- source/utility.containers/ContainerFacadeSpecs.cs
+++ source/utility.containers/ContainerFacadeSpecs.cs
@@ -15,20 +15,25 @@ namespace code.utility.containers
 
     public class when_fetching_a_dependency : concern
     {
-        private Establish c = () =>
+        public class SomeItem
+        {
+            
+        }
+
+      private Establish c = () =>
       {
         registry = depends.on<IFindFactoriesForAType>();
-        registry.setup(x => x.get_resolver_for_type<object>()).Return(the_builder);
-        the_builder = fake.an<TypeBuilder<object>>;
+        registry.setup(x => x.get_resolver_for_type<SomeItem>()).Return(the_builder);
+        the_builder = fake.an<TypeBuilder<SomeItem>>();
       };
 
-      Because b = () => sut.an<Object>();
+      Because b = () => sut.an<SomeItem>();
 
       It uses_the_type_builder_to_build_the_type = () =>
         the_builder.should().received(x => x.Invoke());
 
       static IFindFactoriesForAType registry;
-      static TypeBuilder<object> the_builder;
+      static TypeBuilder<SomeItem> the_builder;
     }
   }
 }
